b96cd4e0c0485b60427c78e128f184c2029494bf
donaldcjapi <dcjapi@gmail.com>
2025-10-23 17:58:13 +0700

    aaaaa

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { visualizer } from "rollup-plugin-visualizer";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
@@ -13,7 +14,17 @@ export default defineConfig(({ mode }) => ({
     host: true,
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+    mode === "production" &&
+      visualizer({
+        open: true, // Opens the report in your browser
+        filename: "./dist/stats.html", // Output file in the dist folder
+        gzipSize: true, // Show gzip sizes
+        brotliSize: true, // Show brotli sizes
+      }),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
